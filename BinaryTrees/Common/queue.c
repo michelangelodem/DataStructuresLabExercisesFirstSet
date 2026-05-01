e99fd74137cf07296cab8e84afb0130fef5c90da
mantellos <mantellosk@gmail.com>
2026-05-01 21:26:01 +0300

    Ekana bool ta headers eixa kanei malakia

--- BinaryTrees/Common/queue.c
+++ BinaryTrees/Common/queue.c
@@ -1,20 +1,20 @@
 #include "queue.h"
 
-void initializeQueue(Queue* q) {
+bool initializeQueue(Queue* q) {
     q->data = (Node**)malloc(sizeof(Node*) * MAX_SIZE);
 
     // Έλεγχος αν η μνήμη δεσμεύτηκε επιτυχώς
     if (q->data == NULL) {
-        return 0; 
+        return false; 
     }
 
     q->front = 0;
     q->back = -1;
     q->count = 0;
-    return 1;
+    return true;
 }
 
-void enqueue(Queue* q, Node* n) {
+bool enqueue(Queue* q, Node* n) {
   if (q->count == MAX_SIZE) { return 0; }
     
     q->back = (q->back + 1) % MAX_SIZE; 
@@ -24,19 +24,19 @@ void enqueue(Queue* q, Node* n) {
     q->data[q->back] = n; 
     
     q->count++; 
-    return 1;
+    return true;
 }
 
-void dequeue(Queue* q) {
+bool dequeue(Queue* q) {
     // Αν η ουρά είναι άδεια, επιστρέφει false
     if (q->count == 0) {
-        return 0; 
+        return false; 
     }
     
     // Κυκλική αύξηση του front
     q->front = (q->front + 1) % MAX_SIZE; 
     q->count--; 
-    return 1;
+    return true;
 }
 
 Node* peek(const Queue* q) {
@@ -47,13 +47,14 @@ Node* peek(const Queue* q) {
     return NULL;
 }
 
-void freeQueue(Queue* q) {
-if (q->data != NULL) {
+bool freeQueue(Queue* q) {
+    if (q->data != NULL) {
         free(q->data);
         q->data = NULL;
     }
-    
+
     q->front = 0;
     q->back = -1;
     q->count = 0;
+    return true;
 }
--- BinaryTrees/Common/queue.h
+++ BinaryTrees/Common/queue.h
@@ -1,8 +1,10 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "tree.h"
+#include <stdbool.h>
 
 #define MAX_SIZE 100
 
@@ -13,9 +15,10 @@ typedef struct Queue {
     int count;
 } Queue;
 
-void initializeQueue(Queue* q);
-void enqueue(Queue* q, Node* n);
-void dequeue(Queue* q);
+bool initializeQueue(Queue* q);
+bool enqueue(Queue* q, Node* n);
+bool dequeue(Queue* q);
 Node* peek(const Queue* q);
-void freeQueue(Queue* q);
+bool freeQueue(Queue* q);
+
 #endif
